f74cc98d9ea4af1422a8683b2266f5d669c623fa
Shrey Jain <31862368+shreyjain711@users.noreply.github.com>
2024-02-19 16:33:06 +0530

    solution balanced_tree.cpp

---
+++ Trees/balanced_tree.cpp
@@ -0,0 +1,11 @@
+// ***Problem Desc***: height-balanced, i.e. for any node, diff b/w height left and right subtrees <= 1
+// ***Sol [O(n) time | O(n) space]***:
+int helper(TreeNode* root, bool &isB) {
+    if (!root) return 0;
+    int leftH = helper(root->left, isB), rightH = helper(root->right, isB);
+    if (abs(leftH - rightH) > 1) isB = 0;
+    return max(leftH, rightH) + 1;
+}
+bool isBalanced(TreeNode* root) {
+    bool isB = 1; helper(root, isB); return isB;
+}

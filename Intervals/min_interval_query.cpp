40536a73303ddd4590edc11e618898b9a5080070
Shrey Jain <31862368+shreyjain711@users.noreply.github.com>
2024-04-09 00:09:13 +0530

    Update min_interval_query.cpp

--- Intervals/min_interval_query.cpp
+++ Intervals/min_interval_query.cpp
@@ -1,21 +1,18 @@
 // ***Problem Desc***: given intervals, not sorted and overlapping, for list of given numbers tell what's the shortest len interval they lie in
 // ***minQ of inters, compared on len; iter on sorted Qs, push inters that began before Q and pop that ended before Q, top is the ans len [O(n.logn + qlogq) time | O(n+q) space]***:
 vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
-    auto comparator = [](vector<int> &a, vector<int> &b) {
-        return (a[1]-a[0]) > (b[1]-b[0]);
-    };
-    priority_queue<vector<int>, vector<vector<int>>, decltype(comparator)> minq(comparator);
-
-    vector<int> ans, sortedQs(queries.begin(), queries.end()); 
-    sort(sortedQs.begin(), sortedQs.end());
-    unordered_map<int, int> m; for(int q:queries) m[q] = -1;
-    queue<vector<int>> st; sort(intervals.begin(), intervals.end());
-
-    int i = 0, n=intervals.size();
+    vector<int> ans(queries.size(), -1); vector<pair<int,int>> sortedQs;
+    for (int i=0; i<queries.size(); ++i) sortedQs.push_back({queries[i], i});
+    sort(begin(sortedQs), end(sortedQs));
+    sort(intervals.begin(), intervals.end());
+    auto comp = [](pair<int,int>&a, pair<int,int>&b) {return a.first > b.first;};
+    priority_queue<pair<int,int>, vector<pair<int,int>>, decltype(comp)> minQ(comp);
+    int i = 0;
     for (auto q: sortedQs) {
-        while (i<n && intervals[i][0] <= q) minq.push(intervals[i++]);
-        while (!minq.empty() && minq.top()[1] < q) minq.pop();
-        if(!minq.empty()) m[q] = minq.top()[1]-minq.top()[0]+1;
-    } for (auto q:queries) ans.push_back(m[q]);
+      while (i < intervals.size() && intervals[i][0] <= q.first) 
+          minQ.push({intervals[i][1] - intervals[i][0] + 1, intervals[i++][1]});
+      while (minQ.size() && minQ.top().second < q.first) minQ.pop();
+      if (minQ.size()) ans[q.second] = minQ.top().first;
+    }
     return ans;
 }

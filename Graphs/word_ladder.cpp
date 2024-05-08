ebca942b32e5b64024153682976cb2842fd327a5
Shrey Jain <31862368+shreyjain711@users.noreply.github.com>
2024-05-08 19:02:36 +0530

    Update word_ladder.cpp

--- Graphs/word_ladder.cpp
+++ Graphs/word_ladder.cpp
@@ -4,26 +4,26 @@
 ***bfs with the starting word, make hashset of wordList, for each possible variation of the current word in set, add to Q remove from set [O(n.wordLen<sup>2</sup>) time | O(n) space]***:
   - can't do bfs and check all words in the set for the next level, that's n<sup>2</sup>.m complexity, since n > m, this is inefficient
 */
-bool isAdj(string a, string b) {
-    int diff = 0; for(int i=0;i<a.size();++i) if (a[i]!=b[i]) diff++;
-    return diff == 1;
-}
 int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-    int ans = 1; queue<string> q; q.push(beginWord);
-    unordered_set<string> wordSet(wordList.begin(), wordList.end());
-
-    while (!q.empty()) {
-        ans++; int sz = q.size();
-        for (int i=0; i<sz; i++) {
-            string currWord = q.front(), temp; q.pop();
-            for(int l=0; l<currWord.size(); l++) {
-                temp = currWord;
-                for(char c='a'; c<='z'; c++) {
-                    temp[l] = c;
-                    if (wordSet.find(temp) == wordSet.end()) continue;
-                    if (isAdj(currWord, temp)) {
-                        if (temp == endWord) return ans;
-                        q.push(temp);
-                        wordSet.erase(temp);
-    }}}}} return 0;
+    int ans = 1; unordered_set<string> wordSet(begin(wordList), end(wordList)); 
+    if (!wordSet.count(endWord)) return 0;
+    queue<string> bfs; bfs.push(beginWord);
+    while (bfs.size()) {
+        int n = bfs.size(); ans++;
+        for (int i = 0; i<n; ++i) {
+            string s = bfs.front(); bfs.pop(); 
+            for (int j=0; j<s.size(); ++j) {
+                char c = s[j];
+                for (char rep = 'a'; rep<='z'; ++rep) {
+                    if (rep!=c) {
+                        s[j] = rep;
+                        if (wordSet.count(s)) {
+                            if (s==endWord) return ans;
+                            bfs.push(s); wordSet.erase(s);
+                        }
+                    }
+                } s[j] = c;
+            }
+        } 
+    } return 0;
 }
